3f4103acba2089438eb75227a6a87c5b84ee308c
EdgarVasquez <edgarvasquezpe@gmail.com>
2022-06-15 19:14:28 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+
+def RangeInteger1(number):
+
+    if number >= 2 and number < 6:
+        return  True
+    else:
+        return  False
+
+def ContainsRange(number):
+    print("hello")
+
+if __name__ == '__main__':
+    RangeInteger(1)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
