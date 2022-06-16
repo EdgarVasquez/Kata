9d872206bccc02a096745ea4dbb67ee31d401fbb
EdgarVasquez <edgarvasquezpe@gmail.com>
2022-06-15 20:01:07 -0400

    commit 3

--- main.py
+++ main.py
@@ -18,6 +18,23 @@ def ContainsRange2(number):
     else:
         return False
 
+def ContainsRange3(number):
+    if number >= 2 and number < 10:
+        return True
+    else:
+        return False
+def GetAllPoints(RANGE1,RANGE2):
+    i = 0
+    resultado = ""
+    while i < RANGE2:
+         if i >= RANGE1 and i < RANGE2:
+             resultado = resultado +","+ str(i)
+             i = i +1
+         else:
+             i = i + 1
+    return  resultado
+
+
 if __name__ == '__main__':
     RangeInteger(1)
 
--- test.py
+++ test.py
@@ -3,16 +3,31 @@ import main
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+        self.assertEqual(True, main.RangeInteger1(2))  # add assertion here pasar
 
     def test_ContainRange(self):
-        self.assertEqual(True, main.ContainsRange(7))  # add assertion here
+        self.assertEqual(False, main.ContainsRange(7))  # add assertion here no va a pasar
 
     def test_ContainRange2(self):
-        self.assertEqual(True, main.ContainsRange(10))
+        self.assertEqual(False, main.ContainsRange(10)) # no va a pasar
 
     def test_ContainRange3(self):
-        self.assertEqual(True, main.ContainsRange2(2))
+        self.assertEqual(False, main.ContainsRange2(2)) # no va pasar
+
+    def test_ContainRange4(self):
+        self.assertEqual(True, main.ContainsRange3(3)) #  va pasar
+
+    def test_ContainRange5(self):
+            self.assertEqual(True, main.ContainsRange3(5))  # va pasar
+
+    def test_ContainRange6(self):
+        self.assertEqual(False, main.ContainsRange3(11))  # no va pasar
+
+    def test_ContainRange7(self):
+        self.assertEqual(True, main.ContainsRange3(3))  # va pasar
+
+    def test_ContainRange9(self):
+            self.assertEqual(",2,3,4,5", main.GetAllPoints(2,6))  # va pasar
 
 
 if __name__ == '__main__':
